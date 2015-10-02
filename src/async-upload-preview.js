e97bcf53d3fc07f4bf4022d814be493973ed6d36
Leandro Segovia <ldlsegovia@gmail.com>
2015-10-02 16:20:59 -0300

    feat(directives.AsyncUploadPreview): put single file functions in own scope

--- src/async-upload-preview.js
+++ src/async-upload-preview.js
@@ -13,6 +13,7 @@ function asyncUploadPreview() {
       ngModel: '=',
       documentName: '=',
       uploadUrl: '@',
+      multiple: '@',
       renderImageAs: '@',
       noDocumentText: '@',
       documentExtension: '@',
@@ -22,6 +23,7 @@ function asyncUploadPreview() {
       '<div>' +
         '<async-upload ' +
           'upload-url="{{uploadUrl}}" ' +
+          'multiple="{{multiple}}" ' +
           'start-callback="setInitialState()" ' +
           'progress-callback="setProgress(event)" ' +
           'success-callback="setUploadData(uploadData)" ' +
@@ -29,17 +31,20 @@ function asyncUploadPreview() {
           'remove-callback="setInitialState()" ' +
           'ng-model="ngModel">' +
         '</async-upload>' +
-        '<upload-progress type="{{progressType}}" ' +
+        '<upload-progress ' +
+          'ng-if="!multiple" ' +
+          'type="{{progressType}}" ' +
           'hide-on-complete="true" ' +
           'hide-on-zero="true" ' +
-          'progress-data="progressData">' +
+          'progress-data="sMode.progressData">' +
         '</upload-progress>' +
         '<doc-preview ' +
+          'ng-if="!multiple" ' +
           'no-document-text="{{noDocumentText}}" ' +
           'render-image-as="{{renderImageAs}}" ' +
-          'document-name="uploadData.documentName" ' +
-          'document-extension="{{uploadData.fileExtension}}" ' +
-          'document-url="uploadData.downloadUrl">' +
+          'document-name="sMode.uploadData.documentName" ' +
+          'document-extension="{{sMode.uploadData.fileExtension}}" ' +
+          'document-url="sMode.uploadData.downloadUrl">' +
         '</doc-preview>' +
       '<div>'
   };
@@ -52,37 +57,60 @@ function asyncUploadPreview() {
     _scope.setError = setError;
     _scope.setInitialState = setInitialState;
 
-    function setProgress(event) {
-      if(event) {
-        _scope.progressData = event;
+    _scope.mMode = {
+      setProgress: function(event) {
+      },
+
+      setError: function(errorData) {
+      },
+
+      setInitialState: function() {
+      },
+
+      setUploadData: function(uploadData) {
       }
-    }
+    };
 
-    function setError(errorData) {
-      _scope.uploadData = null;
-      _scope.progressData.error = true;
-    }
+    _scope.sMode = {
+      setProgress: function(event) {
+        if(event) {
+          _scope.sMode.progressData = event;
+        }
+      },
 
-    function setInitialState() {
-      _scope.uploadData = null;
-      _scope.progressData = { loaded: 0, total: 1, error: false };
-    }
+      setError: function(errorData) {
+        _scope.sMode.uploadData = null;
+        _scope.sMode.progressData.error = true;
+      },
 
-    function setUploadData(uploadData) {
-      var data = (uploadData.upload || uploadData);
+      setInitialState: function() {
+        _scope.sMode.uploadData = null;
+        _scope.sMode.progressData = { loaded: 0, total: 1, error: false };
+      },
 
-      _scope.uploadData = {
-        identifier: data.identifier,
-        documentName: (data.file_name || data.fileName),
-        fileExtension: (data.file_extension || data.fileExtension),
-        downloadUrl: (data.download_url || data.downloadUrl)
-      };
+      setUploadData: function(uploadData) {
+        var data = (uploadData.upload || uploadData);
 
-      if(!_scope.uploadData.identifier || !_scope.uploadData.documentName ||
-        !_scope.uploadData.fileExtension || !_scope.uploadData.downloadUrl) {
-        throw 'Invalid response. Must be a json with identifier, file_name, file_extension and download_url';
+        _scope.sMode.uploadData = {
+          identifier: data.identifier,
+          documentName: (data.file_name || data.fileName),
+          fileExtension: (data.file_extension || data.fileExtension),
+          downloadUrl: (data.download_url || data.downloadUrl)
+        };
+
+        if(!_scope.sMode.uploadData.identifier || !_scope.sMode.uploadData.documentName ||
+          !_scope.sMode.uploadData.fileExtension || !_scope.sMode.uploadData.downloadUrl) {
+          throw 'Invalid response. Must be a json with identifier, file_name, file_extension and download_url';
+        }
       }
-    }
+    };
+
+    var mode = !!_scope.multiple ? _scope.mMode : _scope.sMode;
+
+    function setProgress(event) { mode.setProgress(event); }
+    function setError(errorData) { mode.setError(errorData); }
+    function setInitialState() { mode.setInitialState(); }
+    function setUploadData(uploadData) { mode.setUploadData(uploadData); }
   }
 }
 
