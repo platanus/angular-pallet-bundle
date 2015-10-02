9d652077e2e506f53e35ece437da34dfc60a80b3
Leandro Segovia <ldlsegovia@gmail.com>
2015-10-02 17:25:08 -0300

    feat(directives.AsyncUploadPreview): handle upload for multiple files
    show preview only, no progress or smart error handling yet

--- src/async-upload-preview.js
+++ src/async-upload-preview.js
@@ -24,40 +24,52 @@ function asyncUploadPreview() {
         '<async-upload ' +
           'upload-url="{{uploadUrl}}" ' +
           'multiple="{{multiple}}" ' +
-          'start-callback="setInitialState()" ' +
-          'progress-callback="setProgress(event)" ' +
-          'success-callback="setUploadData(uploadData)" ' +
-          'error-callback="setError(errorData)" ' +
-          'remove-callback="setInitialState()" ' +
+          'start-callback="onUploadStart()" ' +
+          'progress-callback="onUploadProgress(event)" ' +
+          'success-callback="onUploadSuccess(uploadData)" ' +
+          'error-callback="onUploadError(errorData)" ' +
+          'remove-callback="onUploadStart()" ' +
           'ng-model="ngModel">' +
         '</async-upload>' +
-        '<upload-progress ' +
-          'ng-if="!multiple" ' +
-          'type="{{progressType}}" ' +
-          'hide-on-complete="true" ' +
-          'hide-on-zero="true" ' +
-          'progress-data="sMode.progressData">' +
-        '</upload-progress>' +
-        '<doc-preview ' +
-          'ng-if="!multiple" ' +
-          'no-document-text="{{noDocumentText}}" ' +
-          'render-image-as="{{renderImageAs}}" ' +
-          'document-name="sMode.uploadData.documentName" ' +
-          'document-extension="{{sMode.uploadData.fileExtension}}" ' +
-          'document-url="sMode.uploadData.downloadUrl">' +
-        '</doc-preview>' +
+        '<div class="single-wrapper" ng-if="!multiple">' +
+          '<upload-progress ' +
+            'type="{{progressType}}" ' +
+            'hide-on-complete="true" ' +
+            'hide-on-zero="true" ' +
+            'progress-data="sMode.progressData">' +
+          '</upload-progress>' +
+          '<doc-preview ' +
+            'no-document-text="{{noDocumentText}}" ' +
+            'render-image-as="{{renderImageAs}}" ' +
+            'document-name="sMode.uploadData.documentName" ' +
+            'document-extension="{{sMode.uploadData.fileExtension}}" ' +
+            'document-url="sMode.uploadData.downloadUrl">' +
+          '</doc-preview>' +
+        '</div>' +
+        '<div class="multiple-wrapper" ng-if="!!multiple">' +
+          '<div class="files-table" ng-repeat="file in mMode.files">' +
+            '<doc-preview ' +
+              'render-image-as="link" ' +
+              'document-name="file.documentName" ' +
+              'document-extension="{{file.fileExtension}}" ' +
+              'document-url="file.downloadUrl">' +
+            '</doc-preview>' +
+          '</div>' +
+        '</div>' +
       '<div>'
   };
 
   return directive;
 
   function link(_scope) {
-    _scope.setUploadData = setUploadData;
-    _scope.setProgress = setProgress;
-    _scope.setError = setError;
-    _scope.setInitialState = setInitialState;
+    _scope.onUploadSuccess = onUploadSuccess;
+    _scope.onUploadProgress = onUploadProgress;
+    _scope.onUploadError = onUploadError;
+    _scope.onUploadStart = onUploadStart;
 
     _scope.mMode = {
+      files: [],
+
       setProgress: function(event) {
       },
 
@@ -65,9 +77,13 @@ function asyncUploadPreview() {
       },
 
       setInitialState: function() {
+        _scope.mMode.files.length = 0;
       },
 
       setUploadData: function(uploadData) {
+        var newFile = {};
+        _scope.mMode.files.push(newFile);
+        setUploadData(newFile, uploadData);
       }
     };
 
@@ -79,38 +95,40 @@ function asyncUploadPreview() {
       },
 
       setError: function(errorData) {
-        _scope.sMode.uploadData = null;
+        _scope.sMode.uploadData = {};
         _scope.sMode.progressData.error = true;
       },
 
       setInitialState: function() {
-        _scope.sMode.uploadData = null;
+        _scope.sMode.uploadData = {};
         _scope.sMode.progressData = { loaded: 0, total: 1, error: false };
       },
 
       setUploadData: function(uploadData) {
-        var data = (uploadData.upload || uploadData);
-
-        _scope.sMode.uploadData = {
-          identifier: data.identifier,
-          documentName: (data.file_name || data.fileName),
-          fileExtension: (data.file_extension || data.fileExtension),
-          downloadUrl: (data.download_url || data.downloadUrl)
-        };
-
-        if(!_scope.sMode.uploadData.identifier || !_scope.sMode.uploadData.documentName ||
-          !_scope.sMode.uploadData.fileExtension || !_scope.sMode.uploadData.downloadUrl) {
-          throw 'Invalid response. Must be a json with identifier, file_name, file_extension and download_url';
-        }
+        setUploadData(_scope.sMode.uploadData, uploadData);
       }
     };
 
     var mode = !!_scope.multiple ? _scope.mMode : _scope.sMode;
 
-    function setProgress(event) { mode.setProgress(event); }
-    function setError(errorData) { mode.setError(errorData); }
-    function setInitialState() { mode.setInitialState(); }
-    function setUploadData(uploadData) { mode.setUploadData(uploadData); }
+    function setUploadData(holder, uploadData) {
+      var data = (uploadData.upload || uploadData);
+
+      holder.identifier = data.identifier;
+      holder.documentName =(data.file_name || data.fileName);
+      holder.fileExtension = (data.file_extension || data.fileExtension);
+      holder.downloadUrl = (data.download_url || data.downloadUrl);
+
+      if(!holder.identifier || !holder.documentName ||
+        !holder.fileExtension || !holder.downloadUrl) {
+        throw 'Invalid response. Must be a json with identifier, file_name, file_extension and download_url';
+      }
+    }
+
+    function onUploadProgress(event) { mode.setProgress(event); }
+    function onUploadError(errorData) { mode.setError(errorData); }
+    function onUploadStart() { mode.setInitialState(); }
+    function onUploadSuccess(uploadData) { mode.setUploadData(uploadData); }
   }
 }
 
